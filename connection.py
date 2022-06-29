b9aa8e10f68eddf89deed4a343f8e59e855c012f
Yusuf Tunc <83668748+yusuftunc1@users.noreply.github.com>
2022-06-29 22:31:21 +0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -3,6 +3,6 @@ import mysql.connector
 connection = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "100.HvUM",
+    password = "********",
     database = "neworder"
 )
